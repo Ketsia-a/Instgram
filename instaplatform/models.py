b470dd9febeca10da85049fa202dfe700364e6a0
Ketsia-a <iraketsia2001@gmail.com>
2021-01-17 22:43:13 +0200

    created classes and made migrations

--- instaplatform/models.py
+++ instaplatform/models.py
@@ -1,3 +1,33 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    bio = models.TextField(max_length=500, blank=True, default='No bio')
+    pp = models.ImageField(upload_to='profiles/', default='a.png')
+
+    def __str__(self):
+        return self.user.username
+
+class Post(models.Model):
+    image = models.ImageField(upload_to='posts/')
+    name = models.CharField(max_length=250, blank=True)
+    caption = models.CharField(max_length=250, blank=True)
+    date = models.DateTimeField(auto_now_add=True, null=True)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
+    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    def __str__(self):
+        return self.name    
+
+class Comment(models.Model):
+    comment = models.TextField()
+    post = models.ForeignKey(Post, on_delete=models.CASCADE, related_name='comments')
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
+    created = models.DateTimeField(auto_now_add=True, null=True)
+    
+    def __str__(self):
+        return self.comment
+
+    class Meta:
+        ordering = ["-pk"]
