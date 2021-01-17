3174f26ab2f945f6514f410a442bcceaa3ac1435
Ketsia-a <iraketsia2001@gmail.com>
2021-01-17 22:40:55 +0200

    added the url to the app

--- instagram/urls.py
+++ instagram/urls.py
@@ -18,9 +18,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('',include('instaplatform.urls')),
-    path('accounts/', include('registration.backends.simple.urls')),
+    path('',include('instaplatform.urls'))
+    # path('accounts/', include('registration.backends.simple.urls')),
 ]
-if settings.DEBUG:
-    urlpatterns+= static(settings.MEDIA_URL, document_root = settings.MEDIA_ROOT)
-
