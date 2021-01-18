e56bbbf056aac4a2c4e09ddae89c6ab3204cdc60
Ketsia-a <iraketsia2001@gmail.com>
2021-01-18 11:20:05 +0200

    added methods to all classes and added username to profile

--- instaplatform/models.py
+++ instaplatform/models.py
@@ -3,9 +3,34 @@ from django.contrib.auth.models import User
 
 # Create your models here.
 class Profile(models.Model):
+    username = models.CharField(max_length=100, blank=True)
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     bio = models.TextField(max_length=500, blank=True, default='No bio')
     pp = models.ImageField(upload_to='profiles/', default='a.png')
+    date = models.DateTimeField(auto_now_add=True, null=True)
+
+
+    @classmethod
+    def get_all_instagram_users(cls):
+        insta_users = cls.objects.all()
+
+        return insta_users
+
+    def save_profile(self):
+        self.save()
+
+    def delete_profile():
+        self.delete()
+
+    @classmethod
+    def update_profile(cls, id, value):
+        cls.objects.filter(id = id).update(user_id = new_user)
+
+    @classmethod
+    def search_by_profile(cls, username):
+        certain_user = cls.objects.filter(user__username__icontains = username)
+
+        return certain_user
 
     def __str__(self):
         return self.user.username
@@ -15,10 +40,31 @@ class Post(models.Model):
     name = models.CharField(max_length=250, blank=True)
     caption = models.CharField(max_length=250, blank=True)
     date = models.DateTimeField(auto_now_add=True, null=True)
-    user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='posts')
-    likes = models.ManyToManyField(User, related_name='likes', blank=True, )
+    profile = models.ForeignKey(Profile, on_delete=models.CASCADE, null = True)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,null=True)
+  
+    @classmethod
+    def get_all_images(cls):
+        images = cls.objects.all().prefetch_related('comments_set')
+
+        return images
+
+    def save_post(self):
+        self.save()
+
+    def delete_post(self):
+        self.delete()
+
+    @classmethod
+    def update_caption(cls,id,caption):
+        update_image = cls.objects.filter(id = id).update(image_caption = caption)
+
+        return update_image
+
     def __str__(self):
-        return self.name    
+
+        return self.caption
+  
 
 class Comment(models.Model):
     comment = models.TextField()
@@ -26,8 +72,19 @@ class Comment(models.Model):
     user = models.ForeignKey(Profile, on_delete=models.CASCADE, related_name='comments')
     created = models.DateTimeField(auto_now_add=True, null=True)
     
+    def save_comments(self):
+        self.save()
+
+    def delete_comments(self):
+        self.delete()
+
+    def update_comment(self):
+        self.update()
+
     def __str__(self):
-        return self.comment
+
+        return self.posted_by.comment
+    
 
     class Meta:
         ordering = ["-pk"]
