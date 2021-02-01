dcbe1d08b79ccb84f49f7ad4fa6a2ffcd60390c3
Ketsia-a <iraketsia2001@gmail.com>
2021-02-01 05:37:31 +0200

    created views and added routes

--- instaplatform/views.py
+++ instaplatform/views.py
@@ -1,42 +1,46 @@
-from django.shortcuts import render
+from django.shortcuts import render,redirect,get_object_or_404
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
-from .models import Post,Profile,Comment
-from django.http import HttpResponse,Http404
+from .models import Post,Profile,Comment,Follow
+from .forms import UpdateUserProfileForm,PostForm,CommentForm
+from django.views.generic import RedirectView
+from django.http import HttpResponse,Http404,HttpResponseRedirect
 # Create your views here.
 
 @login_required(login_url='/accounts/login/')
 def homepage(request):
-    current_user = request.user
-    userprofile= Profile.objects.filter(id = current_user.id).first()
-    usercomments = Comment.objects.filter(id = current_user.id).first()
+    posts = Post.objects.all()
+    users = User.objects.exclude(id=request.user.id)
+    form = PostForm(request.POST or None, files=request.FILES)      
+    if form.is_valid():
+        post=form.save(commit=False)
+        post.user = request.user.profile
+        post.save()
+        return redirect('homepage')
+    context = {
+        'posts': posts,
+        'form': form,
+        'users':users,
+    }
 
-    return render(request, 'homepage.html', { "userprofile": userprofile, "usercomments": usercomments})
+    return render(request, 'homepage.html',context)
 
-@login_required(login_url='/accounts/login/')
-def add_profile(request):
-    current_user = request.user
-    profile = Profile.objects.filter(id = current_user.id)
+@login_required(login_url='login')
+def profile(request, username):
+    images = request.user.profile.posts.all()
     if request.method == 'POST':
-        form = NewProfileForm(request.POST, request.FILES)
-        if form.is_valid():
-            caption = form.save(commit=False)
-            caption.user = current_user
-            caption.save()
-
-            return redirect('myprofile')
+        prof_form = UpdateUserProfileForm(request.POST, request.FILES, instance=request.user.profile)
+        if prof_form.is_valid():
+            prof_form.save()
+            return redirect(request.path_info)
     else:
-        form = NewProfileForm()
+        prof_form = UpdateUserProfileForm(instance=request.user.profile)
+    context = {
+        'prof_form': prof_form,
+        'images': images,
 
-    return render(request, 'edit.html', {"form": form})
-
-@login_required(login_url='/accounts/login/')
-def user_profile(request):
-    current_user = request.user
-    profile_pictures = Posts.objects.filter(user = current_user)
-    my_profile = Profile.objects.filter(user = current_user).first()
-
-    return render(request, 'profile.html', {"profile_pictures":profile_pictures, "my_profile":my_profile})
+    }
+    return render(request, 'profile.html', context)
 
 @login_required(login_url='/accounts/login/')
 def search_users(request):
@@ -53,58 +57,102 @@ def search_users(request):
 
 
 @login_required(login_url='/accounts/login/')
-def upload_image(request):
-    current_user = request.user
-    user_images = Profile.objects.filter(user = current_user).first()
+def comment(request, pk):
+    image = get_object_or_404(Post, pk=pk)
+    is_liked = False
+    if image.likes.filter(id=request.user.id).exists():
+        is_liked = True
     if request.method == 'POST':
-        form = NewImageForm(request.POST, request.FILES)
-        if form.is_valid():
-            image = form.save(commit=False)
-            image.user = current_user
-            image.profile = user_images
-            image.save()
-
-        return redirect('homepage')
-
-    else:
-        form = NewImageForm()
-
-    return render(request, 'upload.html', {"form": form})
-
-
-@login_required(login_url='/accounts/login/')
-def add_comment(request, post_id):
-    current_user = request.user
-    image_item = Post.objects.filter(id = post_id).first()
-    profiless = Profile.objects.filter( user = current_user.id).first()
-    if request.method == 'POST':
-        form = commentForm(request.POST, request.FILES)
+        form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
-            comment.posted_by = profiless
-            comment.commented_image = image_item
+            comment.post = image
+            comment.user = request.user.profile
             comment.save()
-            return redirect('homepage')
+            return HttpResponseRedirect(request.path_info)
     else:
-        form = commentForm()
+        form = CommentForm()
+    context = {
+        'image': image,
+        'form': form,
+        'is_liked': is_liked,
+        'total_likes': image.total_likes()
+    }
+    return render(request, 'post.html', context)
 
-    return render(request, 'comment.html', {"form": form, "post_id": post_id})
+class PostLikeToggle(RedirectView):
+    def get_redirect_url(self, *args, **kwargs):
+        id = self.kwargs.get('id')
+        obj = get_object_or_404(Post, pk=id)
+        url_ = obj.get_absolute_url()
+        user = self.request.user
+        if user in obj.likes.all():
+            obj.likes.remove(user)
+        else:
+            obj.likes.add(user)
+        return url_
 
-def likes(request,id):
-    likes=0
-    image = Post.objects.get(id=id)
-    image.likes = image.likes+1
-    image.save()
-   
-    return redirect("/")
+
+def like(request):
+    image = get_object_or_404(Post, id=request.POST.get('id'))
+    is_liked = False
+    if image.likes.filter(id=request.user.id).exists():
+        image.likes.remove(request.user)
+        is_liked = False
+    else:
+        image.likes.add(request.user)
+        is_liked = False
+
+    context = {
+        'image': image,
+        'is_liked': is_liked,
+        'total_likes': image.total_likes()
+    }
+    if request.is_ajax():
+        html = render_to_string('like.html', context, request=request)
+        return JsonResponse({'form': html})
+
+def likeView(request,pk):
+     post = get_object_or_404(Post,id=request.POST.get('post_id'))
+     post.likes.add(request.user)
+     return HttpResponseRedirect('/')
+
 
 
 @login_required(login_url='/accounts/login/')
-def users_profile(request, ima_id):
-    current_user = User.objects.filter(id = ima_id).first()
-    profily_images = Post.objects.filter(user = current_user)
-    my_profily = Profile.objects.filter(user = current_user).first()
-    
-    return render(request, 'users.html', {"profily_images":profily_images, "my_profily":my_profily})
+def user_profile(request, username):
+    user_prof = get_object_or_404(User, username=username)
+    if request.user == user_prof:
+        return redirect('user_profile', username=request.user.username)
+    user_posts = user_prof.profile.posts.all()
+    followers = Follow.objects.filter(followers=user_prof.profile)
+    follow_status = None
+    for follower in followers:
+        if request.user.profile == follower.following:
 
+            follow_status = True
+        else:
+            follow_status = False
+    context = {
+        'user_prof': user_prof,
+        'user_posts': user_posts,
+        'followers': followers,
+        'follow_status': follow_status
+    }
+    return render(request, 'users.html', context)
+
+def follow(request, pk):
+    if request.method == 'GET':
+        user = Profile.objects.get(pk=pk)
+        follow = Follow(following=request.user.profile, followers=user)
+        follow.save()
+        
+    return redirect('user_profile', user.user.username)
+    
+def unfollow(request, pk):
+    if request.method == 'GET':
+        user_ = Profile.objects.get(pk=pk)
+        unfollow= Follow.objects.filter(following=request.user.profile, followers=user_)
+        unfollow.delete()
+        return redirect('user_profile', user_.user.username)    
 
